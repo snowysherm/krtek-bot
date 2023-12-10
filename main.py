b587132bef5cdfdd14a3b80efe63b5d9b53a581a
sherm <shermkaboom@gmail.com>
2023-12-10 04:03:18 +0100

    medal api

--- main.py
+++ main.py
@@ -7,37 +7,29 @@ from dotenv import load_dotenv
 intents = discord.Intents.default()
 intents.message_content = True
 activity = discord.Game(name='Ahoj!')
-client = discord.Client(activity=activity, intents=intents)
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents, activity=activity)
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 MEDAL_API = os.getenv("MEDAL_API")
 
 
-@client.event
-async def on_message(message):
-    if "hey" in message.content.lower():
-        await message.channel.send(f"<@!{1}>")
-
-
-@client.event
-async def on_message(message):
-    if "hey" in message.content.lower():
-        headers = {"Authorization": MEDAL_API}
-        r = requests.get(
-            "https://developers.medal.tv/v1/latest?userId=50766636&limit=1",
-            headers=headers)
-        response = r.json()
-        await message.channel.send(response["contentObjects"][0]["directClipUrl"])
-        #await message.channel.send('test')
+@bot.command()
+async def clip(ctx):
+    headers = {"Authorization": MEDAL_API}
+    r = requests.get(
+        "https://developers.medal.tv/v1/latest?userId=50766636&limit=1",
+        headers=headers)
+    response = r.json()
+    await ctx.send(response["contentObjects"][0]["directClipUrl"])
 
 
 @bot.command()
-async def test(ctx, arg):
+async def stefan(ctx, arg):
     count = 0
-    while count >= arg + 1:
-        await ctx.send("123")
+    while count < int(arg):
+        await ctx.send(f"<@!{245656082423218187}>")
+        count += 1
 
 
-client.run(DISCORD_TOKEN)
+bot.run(DISCORD_TOKEN)
